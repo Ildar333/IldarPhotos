7b119d62aa24e110b63a5a9be246af0d070e3e65
ildar333 <ildarildar00741@gmail.com>
2020-11-27 20:10:12 +0300

    Добавьте файлы проекта.

---
+++ IldarPhotoы/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace IldarPhotoы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.btnRotate90Left = new Guna.UI2.WinForms.Guna2Button();
+            this.btnOpenFile = new Guna.UI2.WinForms.Guna2Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.guna2Panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(16)))), ((int)(((byte)(0)))));
+            this.guna2Panel1.BorderRadius = 15;
+            this.guna2Panel1.BorderThickness = 2;
+            this.guna2Panel1.Controls.Add(this.pictureBox1);
+            this.guna2Panel1.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2Panel1.Location = new System.Drawing.Point(23, 151);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            this.guna2Panel1.Size = new System.Drawing.Size(819, 382);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(232, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(382, 382);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(16)))), ((int)(((byte)(0)))));
+            this.guna2Panel2.BorderRadius = 50;
+            this.guna2Panel2.Controls.Add(this.guna2Button2);
+            this.guna2Panel2.Controls.Add(this.guna2Button1);
+            this.guna2Panel2.Controls.Add(this.btnRotate90Left);
+            this.guna2Panel2.Controls.Add(this.btnOpenFile);
+            this.guna2Panel2.Location = new System.Drawing.Point(23, 63);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.ShadowDecoration.Parent = this.guna2Panel2;
+            this.guna2Panel2.Size = new System.Drawing.Size(819, 69);
+            this.guna2Panel2.TabIndex = 1;
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button2.Image")));
+            this.guna2Button2.ImageSize = new System.Drawing.Size(60, 60);
+            this.guna2Button2.Location = new System.Drawing.Point(362, 6);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(65, 59);
+            this.guna2Button2.TabIndex = 3;
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button1.Image")));
+            this.guna2Button1.ImageSize = new System.Drawing.Size(70, 70);
+            this.guna2Button1.Location = new System.Drawing.Point(232, 3);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(59, 59);
+            this.guna2Button1.TabIndex = 2;
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click_1);
+            // 
+            // btnRotate90Left
+            // 
+            this.btnRotate90Left.CheckedState.Parent = this.btnRotate90Left;
+            this.btnRotate90Left.CustomImages.Parent = this.btnRotate90Left;
+            this.btnRotate90Left.FillColor = System.Drawing.Color.Transparent;
+            this.btnRotate90Left.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnRotate90Left.ForeColor = System.Drawing.Color.White;
+            this.btnRotate90Left.HoverState.Parent = this.btnRotate90Left;
+            this.btnRotate90Left.Image = global::IldarPhotoы.Properties.Resources.rightRotate;
+            this.btnRotate90Left.ImageOffset = new System.Drawing.Point(0, 6);
+            this.btnRotate90Left.ImageSize = new System.Drawing.Size(60, 60);
+            this.btnRotate90Left.Location = new System.Drawing.Point(297, 3);
+            this.btnRotate90Left.Name = "btnRotate90Left";
+            this.btnRotate90Left.ShadowDecoration.Parent = this.btnRotate90Left;
+            this.btnRotate90Left.Size = new System.Drawing.Size(59, 59);
+            this.btnRotate90Left.TabIndex = 1;
+            this.btnRotate90Left.Click += new System.EventHandler(this.btnRotate90Left_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.CheckedState.Parent = this.btnOpenFile;
+            this.btnOpenFile.CustomImages.Parent = this.btnOpenFile;
+            this.btnOpenFile.FillColor = System.Drawing.Color.Transparent;
+            this.btnOpenFile.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnOpenFile.ForeColor = System.Drawing.Color.White;
+            this.btnOpenFile.HoverState.Parent = this.btnOpenFile;
+            this.btnOpenFile.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenFile.Image")));
+            this.btnOpenFile.ImageSize = new System.Drawing.Size(60, 60);
+            this.btnOpenFile.Location = new System.Drawing.Point(549, 3);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.ShadowDecoration.Parent = this.btnOpenFile;
+            this.btnOpenFile.Size = new System.Drawing.Size(65, 59);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.Filter = "Image file (*.png,*.jpg,*.JPEG)|*.JPEG *.png .jpg ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 556);
+            this.Controls.Add(this.guna2Panel2);
+            this.Controls.Add(this.guna2Panel1);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.Text = "IldarPhotos";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.guna2Panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private Guna.UI2.WinForms.Guna2Button btnRotate90Left;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+    }
+}
+
---
+++ IldarPhotoы/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace IldarPhotoы
+{
+    public partial class Form1 : MetroForm
+    {
+        private string path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            using (openFile = new OpenFileDialog())
+            {
+                if(openFile.ShowDialog() == DialogResult.OK)
+                {
+                    path = openFile.FileName;
+                    SetImage(path);
+                }
+                
+            }
+        }
+
+        private void SetImage(string path)
+        {
+            var image = new Bitmap(path);
+            pictureBox1.Image = image;
+        }
+
+        private void btnRotate90Left_Click(object sender, EventArgs e)
+        {
+            if(path != "")
+            {
+                pictureBox1.Image = RotateImage90deg(pictureBox1.Image, 90f);
+            }
+            else
+            {
+                MessageBox.Show("You did not chose the picture from file", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        //private Bitmap RotateImage90degRight(Image image,  float deg)
+        //{
+        //    Bitmap bitmap = new Bitmap(image.Height, image.Width);
+        //    bitmap.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+        //    using (Graphics g = Graphics.FromImage(bitmap))
+        //    {
+        //        g.TranslateTransform(2*image.Width,-image.Height );
+        //        g.RotateTransform(deg);
+        //        g.DrawImage(image, image.Width,image.Height);
+        //    }
+        //    return bitmap;
+        //}
+
+        private Bitmap RotateImage90deg(Image image, float deg)
+        {
+            Bitmap bitmap = new Bitmap(image.Height, image.Width);
+            bitmap.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.TranslateTransform(image.Width/2, image.Height/2);
+                g.RotateTransform(deg);
+                g.DrawImage(image,-image.Width/2,-image.Height/2);
+            }
+            return bitmap;
+        }
+
+        private void guna2Button1_Click_1(object sender, EventArgs e)
+        {
+            if (path != "")
+            {
+                pictureBox1.Image = RotateImage90deg(pictureBox1.Image, -90f);
+            }
+            else
+            {
+                MessageBox.Show("You did not chose the picture from file", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            if (path != "")
+            {
+                pictureBox1.Image = RotateImage90deg(pictureBox1.Image, 180f);
+            }
+            else
+            {
+                MessageBox.Show("You did not chose the picture from file", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
